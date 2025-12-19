268b200677bc5ef5529936299873612ee7c554a0
Vojtěch Ožana <97116766+yeee32@users.noreply.github.com>
2025-12-19 22:33:55 +0100

    Delete main directory

--- main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module lab01 {
-    requires transitive javafx.controls;
-    requires javafx.fxml;
-    requires javafx.base;
-    requires javafx.graphics;
-    requires java.desktop;
-    opens lab to javafx.fxml;
-    exports lab;
-}
